d1c3b07c75eae5888512eb85e997b6b88cf1cdcf
hlhols <hlhols313@gmail.com>
2025-01-27 15:52:37 +0300

    start with http server and  basic websocket

--- backend/server/main.go
+++ backend/server/main.go
@@ -1,9 +1,34 @@
 package main
 
 import (
+	"fmt"
+	"net/http"
+	"os"
+	"path/filepath"
+	ws "bomber/websocket"
+
 )
 
-
 func main() {
+	if len(os.Args) != 1 {
+		fmt.Println("Usage: go run . || go run main.go")
+		return
+	}
+
+	fmt.Println("Starting the server...")
+
+	publicDir := filepath.Join("..", "..", "public")
+
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		http.ServeFile(w, r, filepath.Join(publicDir, "index.html"))
+	})
+
+	http.HandleFunc("/ws", ws.WsEndpoint)
+
+
 	// Start the server
+	err := http.ListenAndServe(":8080", nil)
+	if err != nil {
+		fmt.Println("Error starting server:", err)
+	}
 }
--- backend/websocket/socket.go
+++ backend/websocket/socket.go
@@ -1,4 +1,45 @@
-package  bomber
+package bomber
 
-import ()
+import (
+	"fmt"
+	"log"
+	"net/http"
 
+	"github.com/gorilla/websocket"
+)
+
+var upgrader = websocket.Upgrader{
+	ReadBufferSize:  1024,
+	WriteBufferSize: 1024,
+}
+
+func WsEndpoint(w http.ResponseWriter, r *http.Request) {
+  
+	conn , err := upgrader.Upgrade(w, r, nil) // upgrade the connection
+	if err != nil {
+		http.Error(w, "Could not open websocket connection", http.StatusBadRequest)
+		return
+	}
+	
+	reader(conn) // listen constantly for new messages
+
+}
+
+func reader(conn *websocket.Conn) { // listen for new messages
+    for {
+
+        messageType, p, err := conn.ReadMessage() 
+        if err != nil {
+            log.Println(err)
+            return
+        }
+
+        fmt.Println(string(p))
+
+        if err := conn.WriteMessage(messageType, p); err != nil {
+            log.Println(err)
+            return
+        }
+
+    }
+}
