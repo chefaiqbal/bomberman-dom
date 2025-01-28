b6b302d60c8097eb771496a7986cd095d6fdbf88
hlhols <hlhols313@gmail.com>
2025-01-28 13:28:08 +0300

    be more complex in websocket

--- backend/websocket/socket.go
+++ backend/websocket/socket.go
@@ -119,14 +119,52 @@ func HandelMsg(p []byte) {
 }
 
 func HandelChat(msg json.RawMessage){ // for chhattting
+	var chat Chat
+
+	if err := json.Unmarshal(msg, &chat); err != nil {
+		log.Printf("Failed to unmarshal chat message: %v", err)
+		return
+	}
+
+	log.Printf("Chat message: %v", chat.Message)
+
+	broadcast([]byte(chat.Message)) // send it to all users 
 
 }
 
 
 func HandelMove(msg json.RawMessage){ // for moving 
+	var move Move
+
+	if err := json.Unmarshal(msg, &move); err != nil {
+		log.Printf("Failed to unmarshal move message: %v", err)
+		return
+	}
+
+	log.Printf("Move message: %v", move)
 
 }
 
 func HandelBomb(msg json.RawMessage){ // for bombing
+	var bomb Bomb
+
+	if err := json.Unmarshal(msg, &bomb); err != nil {
+		log.Printf("Failed to unmarshal bomb message: %v", err)
+		return
+	}
+
+	log.Printf("Bomb message: %v", bomb)
 	
+}
+
+func broadcast(msg []byte) {
+	mu.Lock()
+	defer mu.Unlock()
+
+	for _, c := range clients {
+		if err := c.conn.WriteMessage(websocket.TextMessage, msg); err != nil {
+			log.Printf("Failed to send message to client: %v", err)
+			return
+		}
+	}
 }
